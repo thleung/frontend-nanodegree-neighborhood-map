7e3dc4fc5ea2a293c1cf58ab18e529dde755364c
thleung <thomassleung@gmail.com>
2015-10-10 00:41:36 -0400

    Neigborhood Google map is displayed on screen

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var defMapOptions = {
+  zoom: 14,
+  center: new google.maps.LatLng(42.4850931, -71.43284)
+};
+var map = new google.maps.Map(document.getElementById('map_canvas'), defMapOptions);
+
+/* ===== Model ===== */
+
+/* ===== View ===== */
+
+/* ===== Octopus ===== */
