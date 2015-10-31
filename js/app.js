11c0bed4b0f9cefc8ae3b1ae05a0a648c1c90373
thleung <thomassleung@gmail.com>
2015-10-31 03:14:24 -0400

    Markers now display correct html links.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var markers = [];
+
 var mapOptions = {
   zoom: 14,
   center: new google.maps.LatLng(42.4850931, -71.43284)
@@ -25,11 +27,13 @@ var Location = function( name, title, address, phone, latitude, longitude, pic, 
 	this.web = ko.observable(web);
 
 	// This is the info window that pops up when marker is clicked
+	// _blank opens link in new tab, not in current tab
 	this.infoHtml = ko.computed(function() {
-		console.log("MMMMMMMMMMMM: " + web);
-  		return '<h4>'+ this.name + this.title + '</h4>' + '<img src=' + this.pic +
-    	'>' + '<br>' + '<a href="' + this.web + '">Visit Site' + '</a><br>';
+		console.log("inside infoHtml, web: " + web);
+  		return '<h4>'+ name + title + '</h4>' + '<img src=' + pic +
+    	'>' + '<br>' + '<a href="' + web + '" target="_blank">Visit Site' + '</a><br>';
  	}, this);
+
 	
 	// Create marker to location on google map
 	this.marker = new google.maps.Marker({
@@ -40,15 +44,21 @@ var Location = function( name, title, address, phone, latitude, longitude, pic, 
     	content: this.infoHtml()
 	});
 
+	console.log("marker name: " + this.marker.name);
+	console.log("marker title: " + this.marker.title);
+
 	//listener to add the information window to each marker
  	google.maps.event.addListener(this.marker, 'click', function() {
-    	infowindow.setContent('<h4>'+ this.name + this.title + '</h4>' + '<img src=' + pic +
-        '>' + '<br>' + '<a href="' + this.web + '">Visit Site' + '</a><br>');
+ 		console.log("marker event web: " + web);
+    	infowindow.setContent('<h4>'+ name + title + '</h4>' + '<img src=' + pic +
+        '>' + '<br>' + '<a href="' + web + '" target="_blank">Visit Site' + '</a><br>');
+        console.log('<a href="' + web + '" target="_blank">Visit Site' + '</a><br>');
         infowindow.open(map, this);
 	});
 }
 
 var locationList = [];
+//locationList = ko.observableArray();
 
 var fourSquareList = {
 	sorrento: '4b33e50bf964a5206c2125e3',
@@ -56,6 +66,33 @@ var fourSquareList = {
 	starbucks: '4b1aa48df964a52032ee23e3',
 	londonpizza: '4aa57324f964a5206a4820e3'
 };
+
+// *********** NEW **********************
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+  	console.log("setMapOnAll at " + i);
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
+}
+
+// ********** END NEW ********************
 
 /* ===== View ===== */
 var displayMapModel = function() {
@@ -65,10 +102,26 @@ var displayMapModel = function() {
 	console.log("inside displayLocation");
 
 	// Markers for map
-	locationList = [new Location("Acton Arboretum", "Park", "Taylor Rd", "(978) 264-9631", 42.480561, -71.434777, "images/arboretum.jpg", "www.actonarboretum.org"),
-					new Location("Discovery Museum", "Museum", "177 Main St", "(978) 264-4200", 42.4647833, -71.4561531, "images/dm_face.jpg", "discoverymuseums.org")
+	locationList = [new Location("Acton Arboretum", "Park", "Taylor Rd", "(978) 264-9631", 42.480561, -71.434777, "images/arboretum.jpg", "http://www.actonarboretum.org"),
+					new Location("Discovery Museum", "Museum", "177 Main St", "(978) 264-4200", 42.4647833, -71.4561531, "images/dm_face.jpg", "http://www.discoverymuseums.org")
 	];
 
+	//function to add a marker to the markers array
+  	var addMarker = function () {
+  		console.log("Inside ADDMARKER");
+    	for (var i = 0; i < locationList.length; i++) {
+    		console.log(i);
+      		var mark = locationList[i].marker;
+      		mark.setMap(map);
+      		console.log("added marker");
+    	}
+ 	};
+
+ 	// add markers to list
+ 	for (var i =0; i < locationList.length; i++) {
+ 		console.log( "adding marker " + i + " for locationList");
+ 		markers.push(locationList[i].marker);
+ 	}
 	console.log("calling for loop for foursquare at length");
 	// Use ajax to call foursquare api to get information regarding places
 	for (var venue in fourSquareList) {
@@ -84,14 +137,18 @@ var displayMapModel = function() {
             	var name = venue.name;
             	var id = venue.id;
             	var location = venue.location;
-            	var categories = venue.categories;
+            	var categories = venue.categories[0].name;
             	var urlVenue = venue.url;
             	var latitude = venue.location.lat;
             	var longitude = venue.location.lng;
             	var address = venue.location.address;
+            	var phone = venue.contact.formattedPhone;
+
             	console.log("name: " + name);
             	console.log("id: " + id);
             	console.log("urlVenue:" + urlVenue);
+            	console.log("categories: " + categories);
+            	console.log("phone: " + phone);
 
             	// Photo image for location
             	var photoUrl;
@@ -106,11 +163,46 @@ var displayMapModel = function() {
             		photoUrl = venuePrefix + 150 + venueSuffix;
             	}
             	console.log("photo url: " + photoUrl);
+
+            	locationList.push(new Location( name, categories, address, latitude, longitude, photoUrl, urlVenue));
+            	//locationList[locationList.length - 1].marker.setMap(map);
+            	console.log("length of locationList: " + locationList.length);
+
+            	// Add markers to map
+				console.log("adding marker");
+				var length = locationList.length - 1;
+				markers.push(locationList[length].marker);
             }
 		});
 	}
+
+	// display markers on map
+	showMarkers();
+
+	self.filter = ko.observable('');
+	self.temp = ko.observableArray();
+	self.search = function () {
+    	map.setCenter({lat: 42.4850931, lng: -71.43284});
+    	infowindow.close();
+    	var filter = self.filter();
+        deleteMarkers();
+        self.places.removeAll();
+    	var len = locations.length;
+    	for (var i = 0; i < len; i++) {
+      		if ((locationList)[i].nameTitle().toLowerCase().indexOf(filter.toLowerCase()) >= 0 ) {
+            	self.temp().push(locations[i]);    
+        	}
+    	}
+    	if (self.temp().length === 0) {
+       		self.locationList.push(new Location('No items match your search', "", '', '', '', ''));
+    	}
+    	self.locationList(self.temp());
+    	self.placeMarkers();
+      
+  	};
 };
 
 /* ===== Octopus ===== */
+
 
 ko.applyBindings(new displayMapModel());
